3c507eaba1d6abd5daf55fe0b228638bf0d4b3e1
EFEX <nicolasm@hotmail.es>
2024-09-05 16:44:20 -0300

    chore(vite): library name

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,14 +2,14 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'src',
+  root: 'docs',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
     lib: {
       entry: resolve(__dirname, 'src/lib/jtabular.js'),
-      name: 'MyLib',
-      fileName: 'my-lib',
+      name: 'jtabular',
+      fileName: 'jtabular',
     },
   },
 })
